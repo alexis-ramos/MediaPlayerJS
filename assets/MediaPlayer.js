ca493aa48bb2bff9b56d36667f8bd689fdaa411d
Alexis Ramos <alexisramoscaballero@gmail.com>
2020-01-04 19:13:51 -0600

    Se crea el boton de mute y se agrega la funcion

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,6 +1,15 @@
 function MediaPlayer(config) {
   this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function() {
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  });
+};
 
 MediaPlayer.prototype.play = function() {
   this.media.play();
@@ -16,6 +25,23 @@ MediaPlayer.prototype.togglePlay = function() {
   } else {
     this.pause();
   }
+};
+
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
+};
+
+MediaPlayer.prototype.toggleMute = function() {
+  if (this.media.muted) {
+    this.unmute();
+  } else {
+    this.mute();
+  }
+  console.log(this);
 };
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
 import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const button = document.querySelector("button");
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-const player = new MediaPlayer({ el: video });
+const button = document.querySelector("#playButton");
+const buttonMute = document.querySelector("#mute");
 button.onclick = () => player.togglePlay();
+buttonMute.onclick = () => player.toggleMute();
